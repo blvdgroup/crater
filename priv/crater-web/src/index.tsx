7fe3b722c9cd324382de3f88bc52ec6bd373ac45
Resi Respati <resir014@gmail.com>
2018-03-14 01:40:29 +0700

    make sure API accepts a JSON type

--- priv/crater-web/src/index.tsx
+++ priv/crater-web/src/index.tsx
@@ -16,9 +16,15 @@ normalize()
 
 const token = document.head.querySelector('meta[name="csrf-token"]')
 
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+// Make sure APi returns a JSON type
+// tslint:disable-next-line:no-string-literal
+axios.defaults.headers.common['Accept'] = 'application/json'
+
 if (token) {
   // If CSRF token is available in <head>, register with Axios
-  axios.defaults.headers.common['X-XSRF-TOKEN'] = token.getAttribute('content')
+  axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content')
 }
 
 const history = createBrowserHistory()
