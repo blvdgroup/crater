4241f2a2c6e3500b3f24537dd5b38033b4f41e2c
Resi Respati <resir014@gmail.com>
2018-03-22 22:32:28 +0700

    part 2

--- priv/crater-web/src/components/snippets/CreateSnippetForm.tsx
+++ priv/crater-web/src/components/snippets/CreateSnippetForm.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { InjectedFormikProps, withFormik } from 'formik'
 // TODO: Replace with Controlled editor and hook up with Formik
 import { UnControlled as CodeMirror } from 'react-codemirror2'
+import styled from 'styled-components'
 import Yup from 'yup'
 
 import FormGroup from '../ui/FormGroup'
 import FormLabel from '../ui/FormLabel'
 import TextInput from '../ui/TextInput'
 import InputFeedback from '../ui/InputFeedback'
+import { colors } from 'styles/variables'
 
 // Dynamic imports with every theme/mode change?
 import 'codemirror/lib/codemirror.css'
@@ -22,6 +24,10 @@ interface CreateSnippetFormValues {
 }
 
 type Props = InjectedFormikProps<{}, CreateSnippetFormValues>
+
+const CodeEditor = styled(CodeMirror)`
+  border: 1px dotted ${colors.grey};
+`
 
 const CreateSnippetForm: React.SFC<Props> = ({
   touched,
@@ -53,17 +59,19 @@ const CreateSnippetForm: React.SFC<Props> = ({
         {touched.title && errors.title && <InputFeedback>{errors.title}</InputFeedback>}
       </FormGroup>
       <FormGroup>
-        <CodeMirror
-          value="var test = 'hello JavaScript!'"
+        <CodeEditor
+          value={values.body}
           options={{
             mode: 'javascript',
             theme: '3024-night',
             lineNumbers: true
           }}
           onChange={(editor, data, value) => {
-            console.log(value)
+            setFieldValue('body', value)
           }}
+          onBlur={handleBlur}
         />
+        {touched.body && errors.body && <InputFeedback>{errors.body}</InputFeedback>}
       </FormGroup>
     </form>
   </div>
@@ -72,7 +80,8 @@ const CreateSnippetForm: React.SFC<Props> = ({
 export default withFormik<{}, CreateSnippetFormValues>({
   mapPropsToValues: props => ({ title: '', description: '', language: '', body: '' }),
   validationSchema: Yup.object().shape({
-    title: Yup.string().required('Title is required')
+    title: Yup.string().required('Title is required'),
+    body: Yup.string().required('Code is required')
   }),
   handleSubmit: (values, { setSubmitting, props }) => {
     console.log(values)
